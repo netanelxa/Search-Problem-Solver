a22bf032d5c5cc5cac46053f2c26a86cecae4c0e
netanel <netanelxa@gmail.com>
2020-01-17 12:52:39 +0200

    try

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     MySerialServer *ms= new MySerialServer();
     ClientHandler *ch=new MyTestClientHandler();
     ms->open(5402,ch);
-    CacheManager<string,string>  *cm = new FileCacheManager<string,string>();
+    CacheManager<string,string>  cm = new FileCacheManager<string,string>();
 
     return 0;
 }
